bdb2ba3cfbac2f067cd856e645e68482d8b3c052
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-20 02:28:05 +0400

    Update Laba6-Main.c

--- Laba6-Main.c
+++ Laba6-Main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "Structures.h"
+#include "Defining.h"
 
+TV* Task(TVArray tva, string city)
+{
+	TVArray TV_arr = { NULL, 0, 0 };
+#if ImportArray == 1
+	Imp_Array();
+#else
+	Enter_Array();
+#endif
+	printf("That's it");
+
+	return 0;
+}
+
+int main()
+{
+	Task()
+
+
+
+	//Enter_TVArr(Tv, size);
+	return 1;
+}
