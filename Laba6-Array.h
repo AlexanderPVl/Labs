59aa5dcd5947f0d31ef9213020e65877881dac75
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-24 21:39:22 +0400

    Create Laba6-Array.h

---
+++ Laba6-Array.h
@@ -0,0 +1,11 @@
+#ifndef _ARRAY_H
+#define _ARRAY_H
+
+#include "Structures.h"
+
+void Free_TV_Array(TV *Tv);
+TVArray DefinedArrays(int Arr_Ind);
+TVArray Make_TVArr(TVArray Rep);
+void PrintTVInfo(TV* Tv);
+
+#endif
