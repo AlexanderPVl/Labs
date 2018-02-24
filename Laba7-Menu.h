119afcfdc26fe2b1b03892b1885883e12d3a2ac0
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-02-25 02:42:29 +0400

    Create Laba7-Menu.h

---
+++ Laba7-Menu.h
@@ -0,0 +1,14 @@
+#ifndef _MENU_H
+#define _MENU_H
+
+#include <stdlib.h>
+#include <string.h>
+
+void MainMenu();
+char GetCommand();
+int Do_Command(char Comm);
+void Command_a();
+void Command_b();
+void PrintCommands();
+
+#endif
