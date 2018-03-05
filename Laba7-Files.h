ea15beded9bcf1c853a13028c5b00e8962144f11
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-03-06 03:30:00 +0400

    Create Laba7-Files.h

---
+++ Laba7-Files.h
@@ -0,0 +1,16 @@
+#ifndef _FILES_H
+#define _FILES_H
+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+int Pow(int a, int b);
+void MakeFile(FILE *f);
+double MakeNumber(FILE* f, char c);
+void WriteToPosition(FILE *f, int Pos, char n);
+void WriteNumber(FILE *f, int Position, int Num);
+void Task_A(char* Name, int N);
+void Task_B();
+
+#endif
