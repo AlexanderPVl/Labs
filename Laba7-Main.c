8536aa08d37cef063c59549469decdee8657aa44
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-03-20 04:29:51 +0400

    Update Laba7-Main.c

--- Laba7-Main.c
+++ Laba7-Main.c
@@ -5,10 +5,13 @@ int main()
 	char* FileName = "f.txt", c;
 
 	printf("Choose task: enter 'a' or 'b'\n");
-	if ((c=getchar()) == 'a')
+	if ((c = getchar()) == 'a')
 		Task_A(FileName, 1);
 	else if (c == 'b')
 		Task_B(FileName);
+	else if (c == 'c')
+		Task_A_Bin();
+		
 	
 	return 0;
 }
