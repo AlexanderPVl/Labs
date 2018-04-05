33e57aa1e6b2c7f1c186e022ad445dffddff2773
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-04-05 09:39:05 +0300

    Update Laba8-Files.c

--- Laba8-Files.c
+++ Laba8-Files.c
@@ -5,69 +5,66 @@
 
 FILE* CreateData(char* DBName, char End)
 {
-    FILE *f = fopen(DBName, "w");
-    char c;
-    while((c = getchar()) != End)
-    {
-        putc(c, f);
-    }
-    fclose(f);
+	FILE *f = fopen(DBName, "w");
+	char c;
+	while ((c = getchar()) != End)
+	{
+		putc(c, f);
+	}
+	fclose(f);
 }
 
 void PrintDataBase(char* DBName)
 {
-    FILE *f = fopen(DBName, "r");
-    printf("Printing database:\n");
-    int c;
-    while ((c = getc(f)) != EOF)
-    {
-        putchar(c);
-    }
+	FILE *f = fopen(DBName, "r");
+	printf("Printing database:\n");
+	int c;
+	while ((c = getc(f)) != EOF)
+	{
+		putchar(c);
+	}
 }
 
 void AddLine(char* DBName)
 {
-    FILE *f = fopen(DBName, "r");
-    if (f == NULL)
-    {
-        fclose(f);
-        CreateData(DBName, ';');
-    }
-    else
-        fclose(f);
-    f = fopen(DBName, "a+");
-    int Num;
-    char Line[15];
-    printf("Enter new line:\n");
-    printf("Enter fly number:\n");
-    scanf("%d", &Num);
-    fprintf(f, "%d", Num);
-    printf("Enter Airport:\n");
-    scanf("%s", Line);
-    fprintf(f, "%s", Line);
-    printf("Enter AviaCompany:\n");
-    scanf("%s", Line);
-    fprintf(f, "%s", Line);
-    printf("Enter plane type:\n");
-    scanf("%s", Line);
-    fprintf(f, "%s", Line);
-    printf("Enter destination time by shedule:\n");
-    scanf("%d", &Num);
-    fprintf(f, "%d", Num);
-    fprintf(f, "%c", ':');
-    scanf("%d", &Num);
-    fprintf(f, "%d", Num);
-    printf("Enter destination time ozjidaemoe:\n");
-    scanf("%d", &Num);
-    fprintf(f, "%d", Num);
-    fprintf(f, "%c", ':');
-    scanf("%d", &Num);
-    fprintf(f, "%d", Num);
-    printf("Enter number of passengers:\n");
-    scanf("%d", &Num);
-    fprintf(f, "%d", Num);
-
-    fclose(f);
+	FILE *f = fopen(DBName, "r");
+	if (f == NULL)
+	{
+		fclose(f);
+		CreateData(DBName, ';');
+	}
+	else
+		fclose(f);
+	f = fopen(DBName, "a+");
+	int Num;
+	char Line[15];
+	printf("Enter new line:\n");
+	printf("Enter fly number:\n");
+	scanf("%d", &Num);
+	fprintf(f, "%d ", Num);
+	printf("Enter Airport:\n");
+	scanf("%s", Line);
+	fprintf(f, "%s ", Line);
+	printf("Enter AviaCompany:\n");
+	scanf("%s", Line);
+	fprintf(f, "%s ", Line);
+	printf("Enter plane type:\n");
+	scanf("%s", Line);
+	fprintf(f, "%s ", Line);
+	printf("Enter destination time by shedule:\n");
+	scanf("%d", &Num);
+	fprintf(f, "%d:", Num);
+	scanf("%d", &Num);
+	fprintf(f, "%d ", Num);
+	printf("Enter destination time ozjidaemoe:\n");
+	scanf("%d", &Num);
+	fprintf(f, "%d:", Num);
+	scanf("%d", &Num);
+	fprintf(f, "%d ", Num);
+	printf("Enter number of passengers:\n");
+	scanf("%d", &Num);
+	fprintf(f, "%d\n", Num);
+	fclose(f);
 }
 
 #endif
