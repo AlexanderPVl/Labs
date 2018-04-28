cbc3aea92d5a1717b49dc45ff0a0008b1a5013cd
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-04-28 20:46:54 +0400

    Update Laba8-Files.h

--- Laba8-Files.h
+++ Laba8-Files.h
@@ -11,11 +11,13 @@
 #define WORD_SIZE 15
 #define RECORD_SIZE 35
 #define CLEAN_CONSOLE system("cls")
+#define SEP ''
 
 void Menu(char* FileName);
 void MakeDataBase(char* FileName);
 void AddRecord(char* FileName, char Sep);
 void PrintInfo(char** Adr, int i);
+char** ScanNData(char* FileName, int *RecordCount, int Free, int N1, int N2);
 char** ScanData(char* FileName, int *RecordCount, int Free);
 void DeleteRecord(char* FileName, int Number);
 void SearchByField(char* FileName);
