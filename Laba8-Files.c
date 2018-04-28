a8fbdeed439e15ddc540a5daf337511039aa0b22
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-04-28 23:45:43 +0400

    Update Laba8-Files.c

--- Laba8-Files.c
+++ Laba8-Files.c
@@ -9,21 +9,21 @@ char* StrSepCpy(char* Don, char* Dest, char Sep) // returns pointer on Separator
 {
 	int i = 0;
 	char c;
-	while ((c = *(Don + i)) != Sep && c != '\0')
-	{
-		Dest[i++] = c;
-	}
+	while ((c = *(Don + i)) != Sep && c != '\0') Dest[i++] = c;
 	Dest[i] = '\0';
-	if (c == '\0')
-		return NULL;
+	if (c == '\0') return NULL;
 	return Don + i;
 }
 
-void SortByField(char* FileName)
+void SortByField(char* FileName, char Sep)
 {
 	char c;
+	char* BufStr = CH_WORD_ARR;
+	char* Str1 = CH_WORD_ARR;
+	char* Str2 = CH_WORD_ARR;
+	char* pointer;
 	char** DB;
-	int Count, N1 = -1, N2 = 19;
+	int Count, N1 = -1, N2 = 19, i, j, BufInt;
 	printf("Choose field\n1 - Record number, 2 - Destination, 3 - Company, 4 - Plane Type\n5 - Shedule Time, 6 - Expected Time, 7 - Passangers\n");
 	while ((c = getch()) > '7' || c < '0'){
 		CLEAN_CONSOLE;
@@ -32,8 +32,28 @@ void SortByField(char* FileName)
 	}
 
 	DB = ScanData(FileName, &Count, 0);
+
+	for (i = 0; i < Count - 1; i++){
+		for (j = i + 1; j < Count; j++)
+		{
+			pointer = StrSepCpy(DB[i], Str1, Sep);
+			StrSepCpy(pointer, Str1, Sep);
+			pointer = StrSepCpy(DB[j], Str2, Sep);
+			StrSepCpy(pointer, Str2, Sep);
+			if (_stricmp(DB[i], DB[j]) < 0)
+			{
+				strcpy(BufStr, DB[j]);
+				strcpy(DB[j], DB[i]);
+				strcpy(DB[i], BufStr);
+			}
+		}
+	}
+
 	PrintInfo(DB, Count);
 	free(DB);
+	free(BufStr);
+	free(Str1);
+	free(Str2);
 }
 
 void MakeDataBase(char* FileName, char Sep)
@@ -45,46 +65,38 @@ void MakeDataBase(char* FileName, char Sep)
 	if (f != NULL){
 		printf("DataBase already exists, do you want to rewrite it? 1 - Yes, 0 - No\n");
 		count = getch();
-		if (count == '0')
-		{
-			fclose(f);
-			return;
-		}
+		if (count == '0'){fclose(f); return;}
 	}
 	f = fopen(FileName, "w");
 
-	printf("Enter number of records:\n");
-	scanf("%d", &count);
+	printf("Enter number of records:\n"); scanf("%d", &count);
 	while (count-- > 0){
 		f = fopen(FileName, "a+");
-		printf("Enter flight number\n");
-		scanf("%d", &i);
+		printf("Enter flight number\n"); scanf("%d", &i);
 		fprintf(f, "%d%c", i, Sep);
-		printf("Enter destination airport\n");
-		scanf("%s", string);
+
+		printf("Enter destination airport\n"); scanf("%s", string);
 		fprintf(f, "%s%c", string, Sep);
-		printf("Enter aviacompany\n");
-		scanf("%s", string);
+
+		printf("Enter aviacompany\n"); scanf("%s", string);
 		fprintf(f, "%s%c", string, Sep);
-		printf("Enter plane type\n");
-		scanf("%s", string);
+
+		printf("Enter plane type\n"); scanf("%s", string);
 		fprintf(f, "%s%c", string, Sep);
-		printf("Enter destination time by shedule:\n");
-		printf("Enter hours:\n");
-		scanf("%s", string);
+
+		printf("Enter destination time by shedule:\nEnter hours:\n"); scanf("%s", string);
 		fprintf(f, "%s:", string);
-		printf("Enter minutes:\n");
-		scanf("%s", string);
+
+		printf("Enter minutes:\n"); scanf("%s", string);
 		fprintf(f, "%s%c", string, Sep);
-		printf("Enter expected destination time:\n");
-		printf("Enter hours:\n");
-		scanf("%s", string);
+
+		printf("Enter expected destination time:\nEnter hours:\n"); scanf("%s", string);
 		fprintf(f, "%s:", string);
-		printf("Enter minutes:\n");
-		scanf("%s", string);
+
+		printf("Enter minutes:\n"); scanf("%s", string);
 		fprintf(f, "%s%c", string, Sep);
-		printf("Enter number of passengers:\n");
-		scanf("%d", &i);
+
+		printf("Enter number of passengers:\n"); scanf("%d", &i);
 		fprintf(f, "%d\n", i);
 	}
 	free(string);
@@ -96,52 +108,44 @@ void AddRecord(char* FileName, char Sep) // adds new record to existing database
 	int i;
 	char* string = (char*)malloc(RECORD_SIZE * INTsize);
 	FILE *f = fopen(FileName, "r");
-	if (f == NULL){
-		printf("DataBase does not exist\n");
-		return;
-	}
-	fclose(f);
-	f = fopen(FileName, "a+");
-	printf("Enter flight number\n");
-	scanf("%d", &i);
+	if (f == NULL){printf("DataBase does not exist\n"); return;}
+	fclose(f); f = fopen(FileName, "a+");
+
+	printf("Enter flight number\n"); scanf("%d", &i);
 	fprintf(f, "%d%c", i, Sep);
-	printf("Enter destination airport\n");
-	scanf("%s", string);
+
+	printf("Enter destination airport\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter aviacompany\n");
-	scanf("%s", string);
+
+	printf("Enter aviacompany\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter plane type\n");
-	scanf("%s", string);
+
+	printf("Enter plane type\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter destination time by shedule:\n");
-	printf("Enter hours:\n");
-	scanf("%s", string);
+
+	printf("Enter destination time by shedule:\nEnter hours:\n"); scanf("%s", string);
 	fprintf(f, "%s:", string);
-	printf("Enter minutes:\n");
-	scanf("%s", string);
+
+	printf("Enter minutes:\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter expected destination time:\n");
-	printf("Enter hours:\n");
-	scanf("%s", string);
+
+	printf("Enter expected destination time:\nEnter hours:\n"); scanf("%s", string);
 	fprintf(f, "%s:", string);
-	printf("Enter minutes:\n");
-	scanf("%s", string);
+
+	printf("Enter minutes:\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter number of passengers:\n");
-	scanf("%d", &i);
+
+	printf("Enter number of passengers:\n"); scanf("%d", &i);
 	fprintf(f, "%d\n", i);
 
-	free(string);
-	fclose(f);
+	free(string); fclose(f);
 }
 
 void PrintInfo(char** Adr, int i) // prints array of strings
 {
 	printf("\nDATABASE:\nRecord Number | Destination | Company | Plane Type | Shedule Time | Expected Time | Passangers\n");
 	int n = 0;
-	for (; n < i; n++)
-		printf("%d) %s\n", n + 1, Adr[n]);
+	for (; n < i; n++) printf("%d) %s\n", n + 1, Adr[n]);
 }
 
 void SearchByField(char* FileName) // prints recorsd with wanted field
@@ -150,29 +154,22 @@ void SearchByField(char* FileName) // prints recorsd with wanted field
 	char Flag = 1;
 	int Num, pass, DBsize, i, j;
 	int BufInt;
-	char* Dest = (char*)malloc(WORD_SIZE * CHARsize);
-	char* Comp = (char*)malloc(WORD_SIZE * CHARsize);
-	char* Type = (char*)malloc(WORD_SIZE * CHARsize);
-	char* time1 = (char*)malloc(WORD_SIZE * CHARsize);
-	char* time2 = (char*)malloc(WORD_SIZE * CHARsize);
-	char* BufStr = (char*)malloc(WORD_SIZE * CHARsize);
+	char* Dest = CH_WORD_ARR;
+	char* Comp = CH_WORD_ARR;
+	char* Type = CH_WORD_ARR;
+	char* time1 = CH_WORD_ARR;
+	char* time2 = CH_WORD_ARR;
+	char* BufStr = CH_WORD_ARR;
 	char** DB;
 
 	printf("Print fields. If you want to skip field, print \"-1\" \n");
-	printf("Enter Flight number:\n");
-	scanf("%d", &Num);
-	printf("Enter destination:\n");
-	scanf("%s", Dest);
-	printf("Enter aviacompany:\n");
-	scanf("%s", Comp);
-	printf("Enter plane type:\n");
-	scanf("%s", Type);
-	printf("Enter time by shedule (hours:minutes):\n");
-	scanf("%s", time1);
-	printf("Enter expected time (hours:minutes):\n");
-	scanf("%s", time2);
-	printf("Enter number of passengers:\n");
-	scanf("%d", &pass);
+	printf("Enter Flight number:\n"); scanf("%d", &Num);
+	printf("Enter destination:\n"); scanf("%s", Dest);
+	printf("Enter aviacompany:\n"); scanf("%s", Comp);
+	printf("Enter plane type:\n"); scanf("%s", Type);
+	printf("Enter time by shedule (hours:minutes):\n"); scanf("%s", time1);
+	printf("Enter expected time (hours:minutes):\n"); scanf("%s", time2);
+	printf("Enter number of passengers:\n"); scanf("%d", &pass);
 
 	DB = ScanData(FileName, &DBsize, 0);
 
@@ -181,36 +178,32 @@ void SearchByField(char* FileName) // prints recorsd with wanted field
 		pointer = DB[i] - 1;
 
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if ((Num != atoi(BufStr)) && (Num != -1))
-			continue;
+		if ((Num != atoi(BufStr)) && (Num != -1)) continue;
+
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if (strcmp(BufStr, Dest) && strcmp(Dest, "-1"))
-			continue;
+		if (strcmp(BufStr, Dest) && strcmp(Dest, "-1")) continue;
+
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if (strcmp(BufStr, Comp) && strcmp(Comp, "-1"))
-			continue;
+		if (strcmp(BufStr, Comp) && strcmp(Comp, "-1")) continue;
+
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if (strcmp(BufStr, Type) && strcmp(Type, "-1"))
-		continue;
+		if (strcmp(BufStr, Type) && strcmp(Type, "-1")) continue;
+
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if (strcmp(BufStr, time1) && strcmp(time1, "-1"))
-			continue;
+		if (strcmp(BufStr, time1) && strcmp(time1, "-1")) continue;
+
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if (strcmp(BufStr, time2) && strcmp(time2, "-1"))
-			continue;
+		if (strcmp(BufStr, time2) && strcmp(time2, "-1")) continue;
+
 		pointer = StrSepCpy(pointer + 1, BufStr, '\t');
-		if (pass != atoi(BufStr) && (pass != -1))
-			continue;
+		if (pass != atoi(BufStr) && (pass != -1)) continue;
+
 		printf("%s\n", DB[i]);
 
 	}
 
-	free(Dest);
-	free(Comp);
-	free(Type);
-	free(time1);
-	free(time2);
-	free(BufStr);
+	free(Dest); free(Comp); free(Type);
+	free(time1); free(time2); free(BufStr);
 //	for (i = 0; i < DBsize; i++)
 //		free(DB[i]);
 	free(DB);
@@ -228,32 +221,30 @@ void RedactRecord(char* FileName, int Number, char Sep)
 	for (; i < Number - 1; i++)
 		fprintf(f, "%s\n", Adr[i]);
 
-	printf("Enter flight number\n");
-	scanf("%d", &i);
+	printf("Enter flight number\n"); scanf("%d", &i);
 	fprintf(f, "%d%c", i, Sep);
-	printf("Enter destination airport\n");
-	scanf("%s", string);
+
+	printf("Enter destination airport\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter aviacompany\n");
-	scanf("%s", string);
+
+	printf("Enter aviacompany\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter plane type\n");
-	scanf("%s", string);
+
+	printf("Enter plane type\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter destination time by shedule:\n");
-	printf("Enter hours:\n");
-	scanf("%s", string);
+
+	printf("Enter destination time by shedule:\nEnter hours:\n"); scanf("%s", string);
 	fprintf(f, "%s:", string);
-	printf("Enter minutes:\n");
-	scanf("%s", string);
+
+	printf("Enter minutes:\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
-	printf("Enter expected destination time:\n");
-	printf("Enter hours:\n");
-	scanf("%s", string);
+
+	printf("Enter expected destination time:\nEnter hours:\n"); scanf("%s", string);
 	fprintf(f, "%s:", string);
-	printf("Enter minutes:\n");
-	scanf("%s", string);
+
+	printf("Enter minutes:\n"); scanf("%s", string);
 	fprintf(f, "%s%c", string, Sep);
+
 	printf("Enter number of passengers:\n");
 	scanf("%d", &i);
 	fprintf(f, "%d\n", i);
@@ -352,8 +343,7 @@ char** ScanData(char* FileName, int *RecordCount, int Free) //reads data from da
 		return NULL;
 	}
 
-	while ((c = getc(f)) != EOF)
-	{
+	while ((c = getc(f)) != EOF){
 		if (c != '\n'){
 			i++;
 		}
@@ -362,8 +352,7 @@ char** ScanData(char* FileName, int *RecordCount, int Free) //reads data from da
 			i = 0;
 			Count++;
 		}
-		else
-		{
+		else{
 			i = 0;
 			Count++;
 		}
@@ -371,28 +360,17 @@ char** ScanData(char* FileName, int *RecordCount, int Free) //reads data from da
 	Count++;
 
 	ArrData = (char**)malloc(Count * sizeof(char*));
-	for (i = 0; i < Count - 1; i++)
-		ArrData[i] = (char*)malloc(Len * sizeof(char));
-
+	for (i = 0; i < Count - 1; i++) ArrData[i] = (char*)malloc(Len * sizeof(char));
 	i = 0;
 	fseek(f, 0, SEEK_SET);
-	while ((c = getc(f)) != EOF)
-	{
+	while ((c = getc(f)) != EOF){
 		if (c != '\n'){
-			if (ArrData[j][i + 1] != EOF)
-			ArrData[j][i++] = c;
-		}
-		else{
-			ArrData[j++][i] = '\0';
-			i = 0;
-		}
+			if (ArrData[j][i + 1] != EOF) ArrData[j][i++] = c;}
+		else{ArrData[j++][i] = '\0'; i = 0;}
 	}
-//	ArrData[j][i] = '\0';
 	fclose(f);
-	if (RecordCount != NULL)
-		*RecordCount = j;// -1;
-	if (Free)
-		free(ArrData);
+	if (RecordCount != NULL) *RecordCount = j;
+	if (Free) free(ArrData);
 	return ArrData;
 }
 
@@ -410,13 +388,10 @@ void DeleteRecord(char* FileName, int Number) // deletes one record from databas
 	char** Adr = NULL;
 	Adr = ScanData(FileName, &count, 0);
 	FILE* f = fopen(FileName, "w");
-	for (; i < Number - 1; i++)
-		fprintf(f, "%s\n", Adr[i]);
 
-	//fseek(f, offset, SEEK_SET);
+	for (; i < Number - 1; i++) fprintf(f, "%s\n", Adr[i]);
+	for (i = Number; i < count; i++) fprintf(f, "%s\n", Adr[i]);
 
-	for (i = Number; i < count; i++)
-		fprintf(f, "%s\n", Adr[i]);
 	fclose(f);
 	free(Adr);
 }
@@ -434,12 +409,10 @@ void Menu(char* FileName, char Sep) // user interface with choice
 		switch (c)
 		{
 		case ('0') :
-			CLEAN_CONSOLE;
-			PrintAnnot();
+			CLEAN_CONSOLE; PrintAnnot();
 			break;
 		case ('1') :
-			AddRecord(FileName, Sep);
-			PrintAnnot();
+			AddRecord(FileName, Sep); PrintAnnot();
 			break;
 		case('2') :
 			Adr = ScanData(FileName, &RecordCount, 0);
@@ -448,43 +421,30 @@ void Menu(char* FileName, char Sep) // user interface with choice
 			PrintAnnot();
 			break;
 		case('3') :
-			printf("Choose record (enter number):\n");
-			scanf("%d", &Num);
+			printf("Choose record (enter number):\n"); scanf("%d", &Num);
 			ScanData(FileName, &RecordCount, 1);
-			if (Num > RecordCount || Num <= 0){
-				printf("Number is incorrect\n");
-				break;
-			}
+			if (Num > RecordCount || Num <= 0){printf("Number is incorrect\n"); break;}
 			DeleteRecord(FileName, Num);
 			PrintAnnot();
 			break;
 		case('4') :
-			printf("Choose record (enter number):\n");
-			scanf("%d", &Num);
+			printf("Choose record (enter number):\n"); scanf("%d", &Num);
 			Adr = ScanData(FileName, &RecordCount, 1);
-			if (Num > RecordCount || Num <= 0){
-				printf("Number is incorrect\n");
-				break;
-			}
+			if (Num > RecordCount || Num <= 0){printf("Number is incorrect\n"); break;}
 			RedactRecord(FileName, Num, Sep);
 			PrintAnnot();
 			break;
 		case('5') :
-			MakeDataBase(FileName, '\t');
-			system("cls");
-			PrintAnnot();
+			MakeDataBase(FileName, '\t'); system("cls"); PrintAnnot();
 			break;
 		case('6') :
-			SearchByField(FileName);
-			PrintAnnot();
+			SearchByField(FileName); PrintAnnot();
 			break;
 		case('7') :
-			SortByField(FileName);
+			SortByField(FileName, Sep); PrintAnnot();
 			break;
 		case(ESC) :
 			return;
-			printf("Smth strange happends\n");
-			break;
 		default:
 			break;
 		}
