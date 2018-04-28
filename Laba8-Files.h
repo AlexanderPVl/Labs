6567c14d594244ce29d022b3aff0ac0291c77b65
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-04-28 23:46:04 +0400

    Update Laba8-Files.h

--- Laba8-Files.h
+++ Laba8-Files.h
@@ -11,7 +11,7 @@
 #define WORD_SIZE 15
 #define RECORD_SIZE 35
 #define CLEAN_CONSOLE system("cls")
-#define SEP ''
+#define CH_WORD_ARR (char*)malloc(WORD_SIZE * CHARsize)
 
 void Menu(char* FileName);
 void MakeDataBase(char* FileName);
@@ -21,4 +21,5 @@ char** ScanNData(char* FileName, int *RecordCount, int Free, int N1, int N2);
 char** ScanData(char* FileName, int *RecordCount, int Free);
 void DeleteRecord(char* FileName, int Number);
 void SearchByField(char* FileName);
+void SortByField(char* FileName, char Sep);
 void PrintAnnot();
