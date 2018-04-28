1c2047069fca184651ce19fab9fa012f7cd85def
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-04-28 04:03:59 +0400

    Update Laba8-Files.h

--- Laba8-Files.h
+++ Laba8-Files.h
@@ -2,9 +2,20 @@
 
 #include <stdio.h>
 #include <malloc.h>
+#include <Windows.h>
+#include <stdlib.h>
+
+#define ESC 27
+#define SHIFT 16
+#define ALT 18
+#define WORD_SIZE 15
+#define RECORD_SIZE 35
 
 void Menu(char* FileName);
+void MakeDataBase(char* FileName);
 void AddRecord(char* FileName, char Sep);
 void PrintInfo(char** Adr, int i);
-char** ScanData(char* FileName, int *RecordCount);
+char** ScanData(char* FileName, int *RecordCount, int Free);
+void DeleteRecord(char* FileName, int Number);
+void SearchByField(char* FileName);
 void PrintAnnot();
