da9fefa7a8f9890efe08df6d659cdc65297a9a57
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-04-28 04:03:53 +0400

    Update Laba8-Main.c

--- Laba8-Main.c
+++ Laba8-Main.c
@@ -2,9 +2,17 @@
 
 void main()
 {
+	//SetConsoleOutputCP(866);
+	if (LOWORD(GetKeyboardLayout(0)) != 0x409) // меняет раскладку клавиатуры на eng (через shift - alt)
+	{
+		keybd_event(SHIFT, 0, 0, 0);
+		keybd_event(ALT, 0, 0, 0);
+		keybd_event(SHIFT, 0, KEYEVENTF_KEYUP, 0);
+		keybd_event(ALT, 0, KEYEVENTF_KEYUP, 0);
+	}
+
 	char* FileName = "DataFile.txt";
 
 	Menu(FileName);
-
 
 }
