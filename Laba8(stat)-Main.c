972138182908f23cacae96aca3dda0cd21a0e492
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-05-04 22:30:25 +0400

    Create Laba8(stat)-Main.c

---
+++ Laba8(stat)-Main.c
@@ -0,0 +1,19 @@
+#include "DataFunc.h"
+
+void main()
+{
+	//SetConsoleOutputCP(866);
+	if (LOWORD(GetKeyboardLayout(0)) != 0x409) // меняет раскладку клавиатуры на eng (через shift - alt)
+	{
+		keybd_event(SHIFT, 0, 0, 0);
+		keybd_event(ALT, 0, 0, 0);
+		keybd_event(SHIFT, 0, KEYEVENTF_KEYUP, 0);
+		keybd_event(ALT, 0, KEYEVENTF_KEYUP, 0);
+	}
+
+	char* FileName = "DataFile.bin";
+	char Sep = '\t';
+
+	Menu(FileName, Sep);
+
+}
