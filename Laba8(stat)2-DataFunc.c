65c30ad1376eb85079bf5c83a530cde8df00b0d9
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-05-15 21:11:18 +0400

    Update Laba8(stat)2-DataFunc.c

--- Laba8(stat)2-DataFunc.c
+++ Laba8(stat)2-DataFunc.c
@@ -7,15 +7,35 @@ size_t CHARPsize = sizeof(char*);
 size_t INTsize = sizeof(int);
 size_t STRsize = sizeof(char[WORD_SIZE]);
 
+void IntScanf(int* Num)
+{
+	char c;
+	int Buf = 0;
+	while ((c = getch()) < '0' || c > '9');
+	while (c != 13)
+	{
+		if (c >= '0' && c <= '9'){
+			putchar(c);
+			Buf = 10 * Buf + (c - '0');
+		}
+		c = getch(c);
+	}
+	putchar('\n');
+	*Num = Buf;
+}
+
 void CheckSizeOfFile(char* FileName)
 {
 	RECORD Record;
 	int COUNT, CURRCOUNT, RS = (int)RECORD_SIZE, i;
-	FILE *f = fopen(FileName, "rb");
+	FILE *f = fopen(FileName, "rb"), *SysFile;
 	if (f == NULL) return;
 	SCAN_COUNT;
+	if (COUNT < 1){ printf("ERROR!"); PUT_COUNT(0); return; }
+
 	fseek(f, 0, SEEK_END); CURRCOUNT = ftell(f) / RS;
 	if (CURRCOUNT < 2 * COUNT){ fclose(f); return; }
+
 	FILE* Buff = fopen("_BufFile.bin", "wb");
 	fseek(f, 0, SEEK_SET);
 	for (i = 0; i < COUNT; i++){
@@ -76,8 +96,9 @@ void SortByField(char* FileName)
 	RECORD Record, BufRecord;
 	char c;
 	int i, j, COUNT, offset, RS = (int)RECORD_SIZE;
-	FILE* f = fopen(FileName, "rb");
+	FILE* f = fopen(FileName, "rb"), *SysFile;
 	if (f == NULL){ printf("DataBase does not exist\n"); return; }
+
 	f = fopen(FileName, "rb+");
 	SCAN_COUNT;
 	printf("Choose field to sort:\n1 - Flight Number, 2 - Destination, 3 - Company, 4 - Plane type, 5 - Time by shedule, 6 - Expected time, 7 - Passengers\n");
@@ -85,32 +106,33 @@ void SortByField(char* FileName)
 	fclose(f);
 }
 
-void MakeDataBase(char* FileName, char Sep) //WORKS
+int MakeDataBase(char* FileName, char Sep) //WORKS
 {
 	RECORD Record;
-	int COUNT;
+	int COUNT = -1;
 	FILE *f = fopen(FileName, "rb"), *SysFile;
 	if (f != NULL){
 		printf("DataBase already exists, do you want to rewrite it? 1 - Yes, 0 - No\n");
 		COUNT = getch();
 		if (COUNT == '0'){ fclose(f); return; }
 	}
+
 	f = fopen(FileName, "wb");
-
-	printf("Enter number of records:\n"); scanf("%d", &COUNT);
-	SysFile = fopen(SYS_FILE, "wb");
-	fwrite(&COUNT, INTsize, 1, SysFile);
-
+	printf("Enter number of records:\n"); IntScanf("%d", &COUNT);
+	PUT_COUNT(COUNT);
 	while (COUNT-- > 0){ SCAN_RECORD; WRITE_N_RECORD(f); }
-	fclose(f); fclose(SysFile);	fflush(f);
+
+	fclose(f); fflush(f);
+	return 1;
 }
 
 void PrintDatabase(char* FileName) //WORKS
 {
 	int COUNT;
 	RECORD Record;
-	FILE *f = fopen(FileName, "rb");
+	FILE *f = fopen(FileName, "rb"), *SysFile;
 	if (f == NULL){ printf("DataBase does not exist\n"); return; }
+
 	SCAN_COUNT;
 	printf("Count = %d\n", COUNT);
 	while (COUNT-- > 0){ READ_N_RECORD(f); PRINT_RECORD; }
@@ -121,8 +143,9 @@ void AddRecord(char* FileName) // adds new record to existing database (WORKS)
 {
 	int COUNT, RS = (int)RECORD_SIZE;
 	RECORD Record;
-	FILE *f = fopen(FileName, "rb");
+	FILE *f = fopen(FileName, "rb"), *SysFile;
 	if (f == NULL){ printf("DataBase does not exist\n"); return; }
+
 	SCAN_INC_COUNT;
 	fclose(f); f = fopen(FileName, "rb+");
 	SCAN_RECORD; WRITE_RECORD(f, RS*(COUNT - 1));
@@ -132,12 +155,13 @@ void AddRecord(char* FileName) // adds new record to existing database (WORKS)
 void SearchByField(char* FileName) // prints recorsd with wanted field
 {
 	RECORD Record, BufRecord;
-	FILE *f;
+	FILE *f, *SysFile;
 	char Flag = 0;
 	int COUNT, j, RS = (int)RECORD_SIZE;
 	SCAN_COUNT;
 	f = fopen(FileName, "rb");
 	if (f == NULL){ printf("DataBase does not exist\n"); return; }
+
 	printf("Enter fields. If field does not matter, print '-1'\n");
 	SCAN_RECORD; BufRecord = Record;
 	for (j = 0; j < COUNT; j++){
@@ -151,14 +175,17 @@ void SearchByField(char* FileName) // prints recorsd with wanted field
 	fclose(f);
 }
 
-void RedactRecord(char* FileName, int Num){
+void RedactRecord(char* FileName){
 	RECORD Record;
-	int COUNT, RS = (int)RECORD_SIZE;
-	FILE* f = fopen(FileName, "rb");
+	int COUNT, RS = (int)RECORD_SIZE, Num;
+	FILE* f = fopen(FileName, "rb"), *SysFile;
 	if (f == NULL){ printf("DataBase does not exist\n"); return; }
+
+	printf("Choose record (enter number):\n"); IntScanf("%d", &Num);
 	f = fopen(FileName, "rb+");
 	SCAN_COUNT;
 	if (Num <= 0 || Num > COUNT){ printf("Wrong number!\n"); fclose(f); return; }
+
 	SCAN_RECORD; WRITE_RECORD(f, RS*(Num - 1));
 	fflush(f); fclose(f);
 }
@@ -169,17 +196,18 @@ void PrintAnnot() // prints user interface in console
 	printf("0 - Clean console\n1 - Add new record\n2 - Print Database\n3 - Delete record\n4 - Redact record\n5 - Make database\n6 - Search\n7 - Sort database by field\nEsc - Exit programm\n\n");
 }
 
-void DeleteRecord(char* FileName, int Number) // deletes one record from database
+void DeleteRecord(char* FileName) // deletes one record from database
 {
 	HANDLE handle;
 	RECORD Record;
-	int i = 0, RS = (int)RECORD_SIZE, COUNT, offset;
-	FILE* f = fopen(FileName, "rb");
+	int i = 0, RS = (int)RECORD_SIZE, COUNT, offset, Number;
+	FILE* f = fopen(FileName, "rb"), *SysFile;
 	if (f == NULL){ printf("DataBase does not exist\n"); return; }
+
+	printf("Choose record (enter number):\n"); IntScanf(&Number);// scanf("%d", &Number);
 	f = fopen(FileName, "rb+");
 	SCAN_DEC_COUNT;
 	if (Number > COUNT + 1 || Number <= 0){ SCAN_INC_COUNT; printf("Wrong number!\n"); return; }
-
 
 	handle = fileno(f);
 	if (Number == COUNT + 1){; fclose(f); return; }
@@ -187,13 +215,14 @@ void DeleteRecord(char* FileName, int Number) // deletes one record from databas
 		READ_RECORD(f, RS*Number);
 		WRITE_RECORD(f, RS*(Number - 1));
 	} while (Number++ < COUNT);
-	fclose(f);
-	fflush(f);
+	fclose(f); fflush(f);
 }
 
-void Menu(char* FileName, char Sep) // user interface with choice
+void Menu(char* FileName) // user interface with choice
 {
-	char c;
+	FILE *SysFile;
+	char c = 0;
+	char* kek[100];
 	int RecordCount, Num, DBsize;
 	PrintAnnot();
 
@@ -211,13 +240,11 @@ void Menu(char* FileName, char Sep) // user interface with choice
 			PrintAnnot();
 			break;
 		case('3') :
-			printf("Choose record (enter number):\n"); scanf("%d", &Num);
-			DeleteRecord(FileName, Num);
+			DeleteRecord(FileName);
 			PrintAnnot();
 			break;
 		case('4') :
-			printf("Choose record (enter number):\n"); scanf("%d", &Num);
-			RedactRecord(FileName, Num);
+			RedactRecord(FileName);
 			PrintAnnot();
 			break;
 		case('5') :
