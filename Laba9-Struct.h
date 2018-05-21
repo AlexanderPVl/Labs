f4af5c8c251ec97d76b03fd497434fac396ac336
AlexanderPVl <32830646+AlexanderPVl@users.noreply.github.com>
2018-05-22 01:25:41 +0400

    Create Laba9-Struct.h

---
+++ Laba9-Struct.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdio.h>
+#include <malloc.h>
+
+struct _NODE
+{
+	struct _NODE* Next;
+	int Deg;
+	int Koeff;
+};
+
+typedef struct
+{
+	int Count;
+	struct _NODE* First;
+	char* Name[10];
+} HEAD;
+
+typedef struct _NODE NODE;
+
+HEAD* MakeList(int count);
+int DeleteList(HEAD* head);
+int AddNode(HEAD* head);
+int PrintList(HEAD* head);
+HEAD* FormNew(HEAD* head1, HEAD* head2);
